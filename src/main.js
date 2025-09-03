4da5ee35a5b868903c525b74c3feaac680647ecf
Haider <muhammed.haido786@gmail.com>
2025-09-03 10:42:29 +0400

    Add Code

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-const http = require('http')
+let http = require('http')
+
+const ReturnCode = 200
+const Port = 80
+const Text = 'Welcome!'
+
+http.createServer(function (req, res) {
+  res.writeHead(ReturnCode, {'Content-Type': 'text/plain'});
+  res.end(Text);
+}).listen(Port);
